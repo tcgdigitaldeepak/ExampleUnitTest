be1c355ef79038cd4a154dc34207676e284f89e0
Deepak <tcgdigital.deepak@gmail.com>
2018-01-16 10:49:09 +0530

    add all files
    all

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+public bool IsPrime(int candidate)
+{
+    if (candidate <2 )
+    {
+        return false;
+    }
+    throw new NotImplementedException("Please create a test first");
+}
+
+[DataTestMethod]
+[DataRow(-1)]
+[DataRow(0)]
+[DataRow(1)]
+public void ReturnFalseGivenValuesLessThan2(int value)
+{
+    var result = _primeService.IsPrime(value);
+
+    Assert.IsFalse(result, $"{value} should not be prime");
+}
+    }
+}
