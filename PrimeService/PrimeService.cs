82c2f5b77fb5a581e245d73a67a0712129b36d54
Deepak <tcgdigital.deepak@gmail.com>
2018-01-16 12:00:39 +0530

    commit2

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,16 +4,14 @@ namespace Prime.Services
 {
     public class PrimeService
     {
-public bool IsPrime(int candidate)
-{
-    if (candidate <2 )
-    {
-        return false;
-    }
-    throw new NotImplementedException("Please create a test first");
-}
-
-
-
+        public bool IsPrime(int candidate)
+        {
+            if (candidate <2 )
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+        
     }
 }
